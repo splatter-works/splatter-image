76d9fb0ca9c1941b82b12a227b36a81666edb4b1
Joseph Gawlik <josephgawlik@berkeley.edu>
2025-01-21 11:21:31 +0100

    bs 1, 1% of data, train=test=val

--- train_network.py
+++ train_network.py
@@ -128,7 +128,7 @@ def main(cfg: DictConfig):
 
     dataset = get_dataset(cfg, "train")
     total_samples = len(dataset)
-    num_samples_first_half = total_samples // 2
+    num_samples_first_half = total_samples // 100
     indices = list(range(num_samples_first_half))
     subset_dataset = Subset(dataset, indices)
     dataloader = DataLoader(subset_dataset, 
@@ -137,16 +137,16 @@ def main(cfg: DictConfig):
                             num_workers=num_workers,
                             persistent_workers=persistent_workers)
 
-    val_dataset = get_dataset(cfg, "val")
-    val_dataloader = DataLoader(val_dataset, 
+    # val_dataset = get_dataset(cfg, "val")
+    val_dataloader = DataLoader(dataset, 
                                 batch_size=1,
                                 shuffle=False,
                                 num_workers=1,
                                 persistent_workers=True,
                                 pin_memory=True)
 
-    test_dataset = get_dataset(cfg, "vis")
-    test_dataloader = DataLoader(test_dataset, 
+    # test_dataset = get_dataset(cfg, "vis")
+    test_dataloader = DataLoader(dataset, 
                                  batch_size=1,
                                  shuffle=True)
     
